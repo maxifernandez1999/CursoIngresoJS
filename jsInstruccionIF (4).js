eff7a61a317a75c483a98316134af180ebb6e512
maxifernandez1999 <60673956+maxifernandez1999@users.noreply.github.com>
2020-02-10 22:02:57 -0300

    IF Ejercicio 4

---
+++ jsInstruccionIF (4).js
@@ -0,0 +1,14 @@
+function mostrar()
+{
+	var edad;
+	//tomo la edad  
+ 	
+ 	edad=document.getElementById('edad').value;
+
+ 	if(edad>12&&edad<18)
+ 	{
+ 		alert("adolescente");
+ 	}
+
+
+}//FIN DE LA FUNCIÓN
