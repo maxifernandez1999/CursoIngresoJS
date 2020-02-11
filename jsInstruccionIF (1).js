c0e4b53457d9506f5cba79280055ea9dbd730f40
maxifernandez1999 <60673956+maxifernandez1999@users.noreply.github.com>
2020-02-10 22:01:34 -0300

    IF Ejercicio 1

---
+++ jsInstruccionIF (1).js
@@ -0,0 +1,16 @@
+function mostrar()
+{	
+	var edad;
+	//tomo la edad  
+	
+	edad=document.getElementById('edad').value;	
+
+	if(edad=="15")
+		{
+			alert("niña bonita");
+		}
+
+
+
+
+}//FIN DE LA FUNCIÓN
