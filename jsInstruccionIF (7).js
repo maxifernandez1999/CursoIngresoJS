0c6c6a4ce87dfa8e48bdcd11078200027b67410c
maxifernandez1999 <60673956+maxifernandez1999@users.noreply.github.com>
2020-02-10 22:04:20 -0300

    IF Ejercicio 7

---
+++ jsInstruccionIF (7).js
@@ -0,0 +1,17 @@
+function mostrar()
+{ 
+	var edad;
+	var estadocivil;
+	//tomo la edad  
+
+	edad=document.getElementById('edad').value;
+	estadocivil=document.getElementById('estadoCivil').value;
+
+		if(edad<18 && estadocivil!="soltero")
+		{
+			alert("Es muy pequeño para NO ser soltero");
+		}
+	
+
+
+}//FIN DE LA FUNCIÓN
