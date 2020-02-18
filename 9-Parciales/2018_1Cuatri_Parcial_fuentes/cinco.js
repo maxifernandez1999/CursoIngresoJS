a16a86a50d456a404349d6814947e8b061a77f27
unknown <fmaximiliano443@gmail.com>
2020-02-18 15:17:01 -0300

    SWITCH ej 5 6 y parcial cinco.js

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,24 @@
 function mostrar()
 {
+	var planeta = prompt("Ingresar planeta");
 
+	switch(planeta)
+	{
+		case "tierra":
+			alert("Aca vivimos");
+			break;
+		case "mercurio":
+		case "venus":
+			alert("Aca hace mas calor");
+			break;
+		case "marte":
+		case "jupiter":
+		case "saturno":
+		case "urano":
+		case "neptuno":
+			alert("Aca hace mas frio");
+			break;
+		default:
+			alert("El planeta no existe");
+	}
 }
