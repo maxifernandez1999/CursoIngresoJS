6e05a98facf0b3c589d5241e9899c8c61a87cf90
maxifernandez1999 <60673956+maxifernandez1999@users.noreply.github.com>
2020-02-17 22:01:36 -0300

    SWITCH Ejercicio 3

---
+++ jsInstruccionSwitch(3).js
@@ -0,0 +1,19 @@
+function mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+//alert (mesDelAño);
+switch(mesDelAño)
+{
+	case "Febrero":
+		alert("Este mes no tiene mas de 29 dias");
+		break;
+	default:
+		alert("Este mes tiene 30 o mas dias");
+}
+	
+	
+
+
+}//FIN DE LA FUNCIÓN
