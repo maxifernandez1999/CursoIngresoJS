3a85c00359060f0a773187ba671b55c35e5466b2
unknown <fmaximiliano443@gmail.com>
2020-02-18 14:42:41 -0300

    EntradaSalida 7 8 9 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var sueldo;
+	var resultado;
+	var descuento;
+
+	sueldo=document.getElementById("importe").value;
+	sueldo=parseInt(sueldo);
+
+	descuento = sueldo * 25 / 100;
+
+	resultado = sueldo - descuento;
+
+	document.getElementById("resultado").value = resultado;
 }
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,65 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var num1;
+	var num2;
+	var resultado;
+
+	num1=document.getElementById('numeroUno').value;
+	num2=document.getElementById("numeroDos").value;
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+
+	resultado = num1 + num2;
+
+	alert("la suma es" + resultado);
 }
 
 function restar()
 {
-	
+	var num1;
+	var num2;
+	var resultado;
+
+	num1=document.getElementById('numeroUno').value;
+	num2=document.getElementById("numeroDos").value;
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+
+	resultado = num1 - num2;
+
+	alert("la resta es" + resultado);
 }
 
 function multiplicar()
 { 
-	
+	var num1;
+	var num2;
+	var resultado;
+
+	num1=document.getElementById('numeroUno').value;
+	num2=document.getElementById("numeroDos").value;
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+
+	resultado = num1 * num2;
+
+	alert("la multiplicacion es" + resultado);
 }
 
 function dividir()
 {
-	
+	var num1;
+	var num2;
+	var resultado;
+
+	num1=document.getElementById('numeroUno').value;
+	num2=document.getElementById("numeroDos").value;
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+
+	resultado = num1 / num2;
+
+	alert("la division es" + resultado);
 }
 
