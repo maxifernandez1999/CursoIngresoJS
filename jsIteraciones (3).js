60aae0037593726696ce74384fad5a7486643564
maxifernandez1999 <60673956+maxifernandez1999@users.noreply.github.com>
2020-02-18 22:02:58 -0300

    WHILE Ejercicio 3

---
+++ jsIteraciones (3).js
@@ -0,0 +1,11 @@
+function mostrar()
+{
+
+var clave = prompt("ingrese el número clave.");
+
+while(clave != "utn750")
+{
+	clave = prompt("error, ingrese nuevamente la clave.");
+}
+
+}//FIN DE LA FUNCIÓN
