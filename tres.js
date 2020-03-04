eb78057340105d89470cc24fccea1fe3ca5d4e09
maxifernandez1999 <60673956+maxifernandez1999@users.noreply.github.com>
2020-03-03 21:43:05 -0300

    parcial 3

--- tres.js
+++ tres.js
@@ -1,13 +1,103 @@
+/*1. promedio de edad
+2. nombre del mas joven
+3. cantidad de hombres
+4. cantidad de mujeres
+5. sexo del importe mas alto
+6. operacion mas utilizada
+7. importe del mas viejo*/
 function mostrar()
 {
- 	var precio=prompt("Ingresar precio");
- 	var descuento=prompt("Ingresar descuento");
- 	var preciofinal;
+	var nombreDelCliente
+	var edad
+	var sexo
+	var tipoDeTransaccion
+	var importe
+	var promedio
+	var contador
+	var nombreDelMasJoven
+	var edadDelMasJoven
+	var respuesta
+	var hombresCantidad
+	var mujeresCantidad
+	var importeMayor
+	var sexoDeImporteMayor
+	var contadorExtraccion
+	var contadorDeposito
 
- 	descuento=precio*descuento/100;
- 	preciofinal=precio-descuento;
+	contadorExtraccion = 0;
+	contadorDeposito = 0;
+	respuesta = "s";
+	contador = 0;
 
- 	document.getElementById('elPrecioFinal').value=preciofinal;
+	while(respuesta=="s")
+	{
+		do
+		{
+			nombreDelCliente = prompt("ingrese el nombre del cliente");
+		}while(!isNaN(nombreDelCliente));
+
+		do
+		{
+			edad = prompt("ingrese edad");
+			edad = parseInt(edad);
+		}while(isNaN(edad) || edad < 18 || edad > 112);
+
+		do
+		{
+			sexo = prompt("ingrese su sexo")
+		}while(!isNaN(sexo) || sexo !="f" && sexo!="m");
+
+		do
+		{
+			tipoDeTransaccion = prompt("ingrese el tipo de transaccion")
+		}while(!isNaN(tipoDeTransaccion) || tipoDeTransaccion!="extraccion" && tipoDeTransaccion != "deposito");
+
+		do
+		{
+			importe = prompt("ingrese su importe");
+			importe = parseInt(importe);
+		}while(isNaN(importe) || importe < 0 || importe > 5000);
+		respuesta = prompt("ingrese s para continuar")
+		contador++;
+
+		EdadSuma = EdadSuma + edad;
+		
+	}
+	promedioEdad = EdadSuma / contador;
+
+	if(contador==0 || edad > edadDelMasJoven)
+	{
+		edadDelMasJoven = edad;
+		nombreDelMasJoven = nombre;
+	}
+
+	if(sexo == "m")
+	{
+		hombresCantidad++;
+	}else
+	{
+		mujeresCantidad++;
+	}
+
+	if(contador==0 || importe > importeMayor)
+	{
+		importeMayor = importe;
+		sexoDeImporteMayor = sexo;
+	}
+
+
+	
+
+	
+	document.write("el promedio de edad es"+ promedioEdad + "<br>");
+	document.write("el nombre de la persona mas joven es"+nombreDelMasJoven+ "<br>");
+	document.write("el numero de hombres es"+ hombresCantidad + "<br>");
+	document.write("el numero de mujeres es"+ mujeresCantidad);
+	document.write("el sexo con importe mas alto es"+ sexoDeImporteMayor + "<br>")
+
+
+
+
 
 
 }
